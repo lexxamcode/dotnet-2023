af11a23ca3fed66c78dbae8daf772b6260a2de08
lexxamcode <lexamishkin@mail.ru>
2023-04-02 14:11:53 +0400

    Удаление лишних комментариев

--- HotelApp/HotelAppServer/Controllers/QueryController.cs
+++ HotelApp/HotelAppServer/Controllers/QueryController.cs
@@ -4,8 +4,6 @@ using HotelAppServer.Repository;
 using HotelDomain;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace HotelAppServer.Controllers;
 
 /// <summary>
